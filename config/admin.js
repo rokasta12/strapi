6bd8e0cee74dad3d09ebcb533e153d8c31b80ad5
bedirhan <celayirbedirhan@gmail.com>
2023-11-08 17:48:05 +0300

    add apiToken.salt

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'e4350524b0689fc3f46e2407dae950e2'),
+    secret: env("ADMIN_JWT_SECRET", "e4350524b0689fc3f46e2407dae950e2"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: "123456789" || env("API_TOKEN_SALT"),
   },
-
 });
